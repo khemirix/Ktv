33fa0da641523da4af6e2eca838a7a199b104c2c
khemirix <61617454+khemirix@users.noreply.github.com>
2026-01-30 19:16:08 +0100

    autoplay fix

--- js/player-modal.js
+++ js/player-modal.js
@@ -32,9 +32,9 @@ const PlayerModal = (function(){
       if (!frame) return;
       
       if (type === 'tv' && id) {
-        frame.src = `https://player.videasy.net/tv/${encodeURIComponent(id)}/${encodeURIComponent(season)}/${encodeURIComponent(episode)}?overlay=true&autoPlay=true&color=e50914&nextEpisode=true&episodeSelector=true`;
+        frame.src = `https://player.videasy.net/tv/${encodeURIComponent(id)}/${encodeURIComponent(season)}/${encodeURIComponent(episode)}?overlay=true&Play=true&color=e50914&nextEpisode=true&episodeSelector=true`;
       } else if (type === 'movie' && id) {
-        frame.src = `https://player.videasy.net/movie/${encodeURIComponent(id)}?overlay=true&autoPlay=true&color=e50914`;
+        frame.src = `https://player.videasy.net/movie/${encodeURIComponent(id)}?overlay=true&Play=true&color=e50914`;
       }
 
       // Request fullscreen after iframe loads
