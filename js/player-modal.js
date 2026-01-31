29b64149214f1d967690c3bc84ca920239093ba1
khemirix <61617454+khemirix@users.noreply.github.com>
2026-01-31 21:12:57 +0100

    same origine

--- js/player-modal.js
+++ js/player-modal.js
@@ -32,7 +32,7 @@ const PlayerModal = (function(){
         // Ensure the iframe can enter fullscreen manually via user interaction
         frame.setAttribute('allow', (frame.getAttribute('allow') || '') + ' fullscreen; autoplay');
         frame.setAttribute('allowfullscreen', '');
-        frame.sandbox='allow-forms';
+        frame.sandbox='allow-same-origin';
         frame.src = `https://www.vidking.net/embed/movie/${encodeURIComponent(id)}?overlay=true&Play=true&color=e50914`;
       }
 
