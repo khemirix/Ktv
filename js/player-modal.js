c2eb7b142620f92e8be8de08d89cd80b83a3d08e
khemirix <61617454+khemirix@users.noreply.github.com>
2026-01-31 21:54:42 +0100

    push

--- js/player-modal.js
+++ js/player-modal.js
@@ -32,7 +32,7 @@ const PlayerModal = (function(){
         frame.setAttribute('allow', (frame.getAttribute('allow') || '') + ' fullscreen; autoplay');
         frame.setAttribute('allowfullscreen', '');
         frame.setAttribute("sandbox", "allow-scripts allow-same-origin allow-forms allow-pointer-lock allow-fullscreen");
-11      frame.style.pointerEvents='none';
+        frame.style.pointerEvents='none';
         frame.src = `https://www.vidking.net/embed/movie/${encodeURIComponent(id)}?overlay=true&Play=true&color=e50914`;
       }
 
