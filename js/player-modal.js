579d12b800a18e3b2e2ed3bd4c286b64c82a5a29
khemirix <61617454+khemirix@users.noreply.github.com>
2026-01-31 21:06:05 +0100

    sandbox

--- js/player-modal.js
+++ js/player-modal.js
@@ -26,11 +26,13 @@ const PlayerModal = (function(){
         // Ensure the iframe can enter fullscreen manually via user interaction
         frame.setAttribute('allow', (frame.getAttribute('allow') || '') + ' fullscreen; autoplay');
         frame.setAttribute('allowfullscreen', '');
+        frame.sandbox ='';
         frame.src = `https://www.vidking.net/embed/tv/${encodeURIComponent(id)}/${encodeURIComponent(season)}/${encodeURIComponent(episode)}?overlay=true&Play=true&color=e50914&nextEpisode=true&episodeSelector=true`;
       } else if (type === 'movie' && id) {
         // Ensure the iframe can enter fullscreen manually via user interaction
         frame.setAttribute('allow', (frame.getAttribute('allow') || '') + ' fullscreen; autoplay');
         frame.setAttribute('allowfullscreen', '');
+        frame.sandbox='';
         frame.src = `https://www.vidking.net/embed/movie/${encodeURIComponent(id)}?overlay=true&Play=true&color=e50914`;
       }
 
